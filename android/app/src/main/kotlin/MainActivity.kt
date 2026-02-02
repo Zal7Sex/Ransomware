910ce5c560649886d903d0d8d6785e5df9f4d893
Zal7Seχ <zallonely969@gmail.com>
2026-02-02 21:07:00 +0700

    Create MainActivity.kt

---
+++ android/app/src/main/kotlin/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.pegasustrasher.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // Kosong - menggunakan FlutterActivity default
+}
